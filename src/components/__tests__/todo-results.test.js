33e09af49e79b6a74cfa35a9429d7d2860f7ea5b
Patrik Pečenka <ppecenka@icloud.com>
2026-03-03 20:11:59 +0100

    fix: fix spacing

--- src/components/__tests__/todo-results.test.js
+++ src/components/__tests__/todo-results.test.js
@@ -4,31 +4,31 @@ import { ref } from 'vue'
 import TodoResults from '../todo-results.vue'
 
 vi.mock('@/composables/useCompletedTodos', () => ({
-  useCompletedTodos: vi.fn(),
+    useCompletedTodos: vi.fn(),
 }))
 
 import { useCompletedTodos } from '@/composables/useCompletedTodos'
 
 describe('TodoResults', () => {
-  beforeEach(() => {
-    vi.clearAllMocks()
-  })
+    beforeEach(() => {
+        vi.clearAllMocks()
+    })
 
-  it('Text is green when value is more or equal to 1', () => {
-    useCompletedTodos.mockReturnValue({ completedTodos: ref(1) })
+    it('Text is green when value is more or equal to 1', () => {
+        useCompletedTodos.mockReturnValue({ completedTodos: ref(1) })
 
-    const wrapper = mount(TodoResults)
+        const wrapper = mount(TodoResults)
 
-    expect(wrapper.find('p').classes()).toContain('green-text')
-    expect(wrapper.find('p').classes()).not.toContain('red-text')
-  })
+        expect(wrapper.find('p').classes()).toContain('green-text')
+        expect(wrapper.find('p').classes()).not.toContain('red-text')
+    })
 
-  it('Text is red when value is 0', () => {
-    useCompletedTodos.mockReturnValue({ completedTodos: ref(0) })
+    it('Text is red when value is 0', () => {
+        useCompletedTodos.mockReturnValue({ completedTodos: ref(0) })
 
-    const wrapper = mount(TodoResults)
+        const wrapper = mount(TodoResults)
 
-    expect(wrapper.find('p').classes()).toContain('red-text')
-    expect(wrapper.find('p').classes()).not.toContain('green-text')
-  })
+        expect(wrapper.find('p').classes()).toContain('red-text')
+        expect(wrapper.find('p').classes()).not.toContain('green-text')
+    })
 })
